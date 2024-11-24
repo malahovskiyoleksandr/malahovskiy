ea1b8cbf3e005173b3882523d4e9334189746561
MAX <maksimkrygliak@gmail.com>
2024-11-24 11:46:07 -0800

    isr

--- app/api/get-data/route.js
+++ app/api/get-data/route.js
@@ -1,48 +1,32 @@
-// // app/api/get-data/route.js
-// import fs from "fs";
-// import path from "path";
+// app/api/get-data/route.js
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
 
-// export async function GET(req) {
-//   try {
-    // const filePath = path.join(
-    //   process.cwd(),
-    //   "app",
-    //   "[locale]",
-    //   "(pages)",
-    //   "home",
-    //   "home.json"
-    // ); // Путь к файлу
-//     const fileData = fs.readFileSync(filePath, "utf-8"); // Читаем файл
-//     const data = JSON.parse(fileData); // Парсим данные
+export async function GET(req) {
+  try {
+    const filePath = path.join(
+      process.cwd(),
+      "app",
+      "[locale]",
+      "(pages)",
+      "home",
+      "home.json"
+    ); // Путь к файлу
+    const fileData = fs.readFileSync(filePath, "utf-8"); // Читаем файл
+    const data = JSON.parse(fileData); // Парсим данные
+    console.log(data)
 
-//     return new Response(JSON.stringify(data), { status: 200 });
-//   } catch (error) {
-//     return new Response(
-//       JSON.stringify({ message: "Ошибка при чтении данных." }),
-//       { status: 500 }
-//     );
-//   }
-// }
-
-// pages/api/getData.js
-import fs from 'fs';
-import path from 'path';
-
-export async function getData() {
-  const filePath = path.join(
-    process.cwd(),
-    "app",
-    "[locale]",
-    "(pages)",
-    "home",
-    "home.json"
-  );
-  const data = fs.readFileSync(filePath, 'utf-8');
-  return JSON.parse(data);
+    // return new Response(JSON.stringify(data), { status: 200 });
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    // return new Response(
+    //   JSON.stringify({ message: "Ошибка при чтении данных." }),
+    //   { status: 500 }
+    // );
+    return NextResponse.json(
+      { message: "Ошибка при получении данных." },
+      { status: 500 }
+    );
+  }
 }
-
-export default async function handler(req, res) {
-  const data = await getData();
-  res.status(200).json(data);
-}
-
