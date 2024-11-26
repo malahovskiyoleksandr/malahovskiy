369e505fbc7fb1e867391b90d6c0f146d4e39f9e
максим <maksimkrygliak@gmail.com>
2024-11-26 20:31:06 +0200

    admin

--- app/intl.js
+++ app/intl.js
@@ -4,7 +4,7 @@ import { createIntl } from '@formatjs/intl';
 
 export default async function getIntl(locale, namespace) {
   return createIntl({
-    locale: locale,
+    locale: locale, 
     // messages: (await import(`@/locales/${locale}/${namespace}.json`)).default
   });
 }
