71a5d62501e76bcc9dc0b98812811e58b89025cf
MAX <maksimkrygliak@gmail.com>
2024-12-09 08:36:55 -0800

    1

--- app/[locale]/(pages)/gallery/industrial/page.js
+++ app/[locale]/(pages)/gallery/industrial/page.js
@@ -6,116 +6,30 @@ import Image from "next/image";
 import PhotoSwipeLightbox from "photoswipe/lightbox";
 import styles from "./industrial.module.scss";
 
-const images = [
-  {
-    id: 1,
-    src: "/gallery/industrial/1.jpg",
-    width: 1600,
-    height: 1295,
-    name: "Image 1",
-  },
-  {
-    id: 2,
-    src: "/gallery/industrial/2.jpg",
-    width: 745,
-    height: 1000,
-    name: "Image 2",
-  },
-  {
-    id: 3,
-    src: "/gallery/industrial/3.jpg",
-    width: 1920,
-    height: 1228,
-    name: "Image 3",
-  },
-  {
-    id: 4,
-    src: "/gallery/industrial/4.jpg",
-    width: 1920,
-    height: 926,
-    name: "Image 4",
-  },
-  {
-    id: 5,
-    src: "/gallery/industrial/5.jpg",
-    width: 1920,
-    height: 2753,
-    name: "Image 5",
-  },
-  {
-    id: 6,
-    src: "/gallery/industrial/6.jpg",
-    width: 1920,
-    height: 1519,
-    name: "Image 6",
-  },
-  {
-    id: 7,
-    src: "/gallery/industrial/7.jpg",
-    width: 1920,
-    height: 2486,
-    name: "Image 7",
-  },
-  {
-    id: 8,
-    src: "/gallery/industrial/8.jpg",
-    width: 1920,
-    height: 1463,
-    name: "Image 8",
-  },
-  {
-    id: 9,
-    src: "/gallery/industrial/9.jpg",
-    width: 1920,
-    height: 1346,
-    name: "Image 9",
-  },
-  {
-    id: 10,
-    src: "/gallery/industrial/10.jpg",
-    width: 1920,
-    height: 1473,
-    name: "Image 10",
-  },
-  {
-    id: 11,
-    src: "/gallery/industrial/11.jpg",
-    width: 8160,
-    height: 5987,
-    name: "Image 11",
-  },
-  {
-    id: 12,
-    src: "/gallery/industrial/12.jpg",
-    width: 6115,
-    height: 8160,
-    name: "Image 12",
-  },
-];
-
 export default function PhotoGallery() {
   const [database, setDatabase] = useState();
 
   const image_listRef = useRef(null);
 
-  // useEffect(() => {
-  //   async function getData() {
-  //     try {
-  //       const response = await fetch("https://api.github.com/repos/malahovskiyoleksandr/malahovskiy/contents/data/database.json");
-  //       if (!response.ok) {
-  //         throw new Error("Ошибка при обращении к API GET");
-  //       }
-  //       const data = await response.json();
-  //       const decodedData = JSON.parse(
-  //         Buffer.from(data.content, "base64").toString("utf-8")
-  //       );
-  //       setDatabase(decodedData);
-  //     } catch (error) {
-  //       console.error("Ошибка: fetch(github-get)", error);
-  //     }
-  //   }
-  //   getData()
-  // }, []);
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch("/api/github-get");
+        if (!response.ok) {
+          throw new Error("Ошибка при обращении к API GET");
+        }
+        const data = await response.json();
+        // const decodedData = JSON.parse(
+        //   Buffer.from(data.content, "base64").toString("utf-8")
+        // );
+        console.log(data)
+        setDatabase(data);
+      } catch (error) {
+        console.error("Ошибка: fetch(github-get)", error);
+      }
+    }
+    getData()
+  }, []); 
 
   // Загружаем данные с API
   // useEffect(() => {
@@ -199,8 +113,7 @@ export default function PhotoGallery() {
   return (
     <div id="gallery" className={styles.image_list} ref={image_listRef}>
       {/* {console.log(database?.gallery?.industrial?.page)} */}
-      {/* {database?.gallery?.industrial?.page.map((image, index) => ( */}
-      {images?.map((image, index) => (
+      {database?.gallery?.industrial?.page.map((image, index) => (
         <Link
           className={styles.image_Link}
           key={index}
@@ -209,7 +122,7 @@ export default function PhotoGallery() {
           data-pswp-height={image.height}
         >
           <Image
-            id={image.id}
+            // id={image.id}
             className={styles.image}
             // onLoad={(e) => console.log(e.target.naturalWidth)} // вызов функции после того как картинка полностью загрузится
             // onError={(e) => console.error(e.target.id)} // Функция обратного вызова, которая вызывается, если изображение не загружается.
@@ -218,7 +131,7 @@ export default function PhotoGallery() {
             // placeholder="blur" // размытие заднего фона при загрузке картинки
             // blurDataURL="/path-to-small-blurry-version.jpg" // если включено свойство placeholder="blur" и картинка без импорта - добавляем сжатое/размытое изображение
             quality={10}
-            priority={true} // если true - loading = 'lazy' отменяеться
+            priority={false} // если true - loading = 'lazy' отменяеться
             // loading="lazy" // {lazy - загрузка картинки в области просмотра} | {eager - немедленная загрузка картинки}
             fill={false} //заставляет изображение заполнять родительский элемент
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // предоставляет информацию о том, насколько широким будет изображение в разных контрольных точках
@@ -226,9 +139,9 @@ export default function PhotoGallery() {
             width={image.width} // задать правильное соотношение сторон адаптивного изображения
             height={image.height}
           />
-          <label htmlFor={image.id} className={styles.image_label}>
+          {/* <label htmlFor={image.id} className={styles.image_label}>
             {image.name}
-          </label>
+          </label> */}
         </Link>
       ))}
     </div>
