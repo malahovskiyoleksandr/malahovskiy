12b53889d4f973e79c5b7444fd3604f5a103b810
максим <maksimkrygliak@gmail.com>
2024-12-28 00:10:45 +0200

    1

--- app/[locale]/(pages)/home/page.js
+++ app/[locale]/(pages)/home/page.js
@@ -1,10 +1,6 @@
-// import { motion } from "framer-motion";
 import Image from "next/image";
-// import mainImage from "@/public/images/mainPhoto.jpg";
 import styles from "./home.module.scss";
 import { NextResponse } from "next/server";
-// import Link from "next/link";
-import Developer from "./developer.png";
 
 export const revalidate = 5;
 
@@ -42,13 +38,74 @@ export async function getData() {
     );
   }
 }
+// export async function getDataPhoto() {
+//   try {
+//     const response = await fetch("/api/github_img-get");
+//     if (!response.ok) {
+//       throw new Error("Помилка завантаження даних");
+//     }
+//     const data = await response.json();
+//     console.log(data)
+//     return data
+//   } catch (error) {
+//     console.error("Ошибка:", error.message);
+//   }
+// }
+export async function getDataPhoto() {
+  try {
+    const response = await fetch(
+      `https://api.github.com/repos/malahovskiyoleksandr/malahovskiy/contents/public`,
+      {
+        method: "GET",
+        cache: "no-store",
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+          Accept: "application/vnd.github.v3+json", // Указываем версию API
+        },
+      }
+    );
+
+    if (!response.ok) {
+      const text = await response.text(); // Получаем тело ошибки
+      console.error(`Ошибка запроса: ${response.status}`, text);
+      return NextResponse.json(
+        { error: `Ошибка при получении данных с GitHub: ${response.statusText}` },
+        { status: response.status }
+      );
+    }
+
+    // Логируем весь ответ
+    // console.log("Response:", response);
+
+    const data = await response.json();
+    // console.log("data", data)
+
+    return data;
+    // return NextResponse.json(data);
+  } catch (error) {
+    console.error("Ошибка сервера:", error);
+    return NextResponse.json(
+      { error: `Ошибка сервера: ${error.message}` },
+      { status: 500 }
+    );
+  }
+}
 
 export default async function Home({ params }) {
   const { locale } = params;
   const person = await getData();
+  const photos = await getDataPhoto();
+  console.log(photos)
+  photos.forEach(file => {
+    if (file.type === 'file') {
+      console.log(`File: ${file.name}, URL: ${file.download_url}`);
+    } else if (file.type === 'dir') {
+      console.log(`Directory: ${file.name}, API URL: ${file.url}`);
+    }
+  });
 
   return (
-    <> 
+    <>
       <section className={styles.main_block}>
         <div className={styles.container_main}>
           <Image
