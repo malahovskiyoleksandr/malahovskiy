37d86a073cbbfa49074f2cb804b4550aec3e661d
MAX <maksimkrygliak@gmail.com>
2025-02-19 12:46:59 +0200

    Initial commit

---
+++ app/sitemap.js
@@ -0,0 +1,13 @@
+export default function sitemap() {
+    return [
+      {
+        url: "https://oleksandrmalakhovskyi.vercel.app/",
+        lastModified: new Date().toISOString(),
+      },
+      {
+        url: "https://oleksandrmalakhovskyi.vercel.app/home",
+        lastModified: new Date().toISOString(),
+      },
+    ];
+  }
+  
